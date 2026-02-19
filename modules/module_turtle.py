813e4cc5331073d2145543939c63651cfa896d6d
Ari <113391785+sariyan-0@users.noreply.github.com>
2026-02-19 12:24:40 +0330

    Added some examples

--- modules/module_turtle.py
+++ modules/module_turtle.py
@@ -1,6 +1,7 @@
 ######################## module_turtle.py ########################
 # This module provides a simple interface to the turtle graphics library.
 import turtle
+import random
 
 screen = turtle.Screen()
 turtle.hideturtle()  # Hide the turtle cursor
@@ -27,10 +28,47 @@ t = turtle.Turtle()
 # for i in range(6):
 #     t.forward(200)
 #     t.left(60)
+#     t.forward(200)
 # ------------------------------
 #create a 36 sided polygon
-for i in range(36):
-    t.forward(20)
-    t.left(10)
+# for i in range(36):
+#     t.forward(20)
+#     t.left(10)
 # ------------------------------
+# create a star art that has infinite loops
+# for i in range(1000):
+#     t.forward(200)
+#     t.left(170)
+#     t.forward(220)
+#     t.left(170)
+#     t.backward(260)
+#     t.right(175)
+# ------------------------------
+# create a spiral art that has infinite loops
+# t.speed(0)  # Set the turtle's speed to the maximum
+# t.color("blue")  # Set the turtle's color to red
+# for j in range(1000000):
+#     for i in range(10):
+#         t.forward(200)
+#         t.left(90)
+#     t.left(1)
+# ------------------------------
+# t.speed(0)
+# lst_colors = ["red", "blue", "green", "yellow", "cyan", "magenta"]
+# for j in range(72):
+#     t.color(random.choice(lst_colors))
+#     for i in range(10):
+#         t.forward(200)
+#         t.left(90)
+#     t.left(5)
+# ------------------------------
+# create a line that rotates around the inside of itself (the value should reduce by 10 each time).
+t.speed(0)
+length = 200
+for i in range(20): 
+    t.forward(length)
+    t.left(90)
+    length = length - 10
+
 turtle.mainloop()
+turtle.exitonclick()
